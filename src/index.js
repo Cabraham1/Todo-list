02f4c8d062e88842886df56d699380848221db55
cabraham1 <christopherabraham8@gmail.com>
2023-02-22 20:18:10 +0100

    Add html files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import _ from "lodash";
-import "./style.css";
+// eslint-disable-next-line no-unused-vars
+import _ from 'lodash';
+import './style.css';
+import anyimage from '../assets/icons/sync.png';
 
+const image = new Image();
+image.src = anyimage;
+
+document.getElementById('img1').appendChild(image);
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,51 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      title: "Development",
+      template: './src/index.html',
+      title: 'Development',
     }),
   ],
 
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
 
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
 
   optimization: {
-    runtimeChunk: "single",
+    runtimeChunk: 'single',
   },
 
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'assets/resource',
       },
     ],
   },
