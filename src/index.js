7236aa1f67970f868242221f0f8df5dad4eafbbd
cabraham1 <christopherabraham8@gmail.com>
2023-02-22 20:45:55 +0100

    Add style files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 // eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
 import './style.css';
-import anyimage from '../assets/icons/sync.png';
 
-const image = new Image();
-image.src = anyimage;
-
-document.getElementById('img1').appendChild(image);
